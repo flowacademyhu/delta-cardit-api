e16ad2f84fe92ea1f7bd07af7e6868e8125d7777
savanyab <savanyab@gmail.com>
2019-01-30 15:55:04 +0100

    added groups.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const cards = require('./controllers/cards');
 const users = require('./controllers/users');
+const groups = require('./controllers/groups');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/cards', cards);
 app.use('/users', users);
+app.use('/groups', groups);
 
 app.listen(8000);
