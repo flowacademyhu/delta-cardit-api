cd903c4191f93b548d6b37745b9bf2e7509e8f28
savanyab <savanyab@gmail.com>
2019-01-30 14:35:40 +0100

    fixed index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cards = require('./controllers/cards');
+const users = require('./controllers/users');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
