736f32b9a83ae9c66c6470fe0bfd4e9a01f8a33f
savanyab <savanyab@gmail.com>
2019-02-20 19:41:56 +0100

    added test cases

--- test/users.spec.js
+++ test/users.spec.js
@@ -78,8 +78,57 @@ describe('Cardit API users tests', function () {
 
     describe('POST /users', function () {
         it('create new user', function (done) {
+            let firstName = 'Elek';
+            let lastName = 'Teszt';
+            let email = 'teszt@elek.hu';
+            let password = '1234';
+            let role = 'student';
+            let GroupId = 1;
+      
+
             request(app)
                 .post('/users')
+                .set('Accept', 'application/json')
+                .set('Authorization', token)
+                .send({firstName, lastName, email, password, role, GroupId})
+                .expect(200)
+                /*.expect((res) => {
+                    expect(res.body.firstname).toBe(firstName);
+                    expect(res.body.lastname).toBe(lastName);
+                    expect(res.body.email).toBe(email);
+                    expect(res.body.password).toBe(password);
+                    expect(res.body.role).toBe(role);
+                    expect(res.body.GroupId).toBe(GroupId);
+                  })*/
+                .end((err) => {
+                    if (err) return done(err);
+                    done();
+                });
+
+        });
+    });
+
+    describe('PUT /users/:id', function () {
+        it('updates user', function (done) {
+            let firstName = 'Ella'
+
+            request(app)
+                .put('/users/1')
+                .set('Accept', 'application/json')
+                .set('Authorization', token)
+                .send({firstName})
+                .expect(200)
+                .end((err) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+    });
+
+    describe('DELETE /users/:id', function () {
+        it('deletes user by id', function (done) {
+            request(app)
+                .delete('/users/1')
                 .set('Accept', 'application/json')
                 .set('Authorization', token)
                 .expect(200)
@@ -89,5 +138,7 @@ describe('Cardit API users tests', function () {
                 });
         });
     });
+
+    
 
 });
